97f6bd2cda47aa826d9ba007e7898447a2ad1402
Adam Slack <adamslack@outlook.com>
2017-08-02 12:05:36 +0100

    Can now geta 404 from the api server and api server logs a response.

--- edi/src/app/app.component.ts
+++ edi/src/app/app.component.ts
@@ -5,7 +5,7 @@
  * Allows the inclusion of other services/functionality needed in later derived components
  */
 import { Component, OnInit, Injectable } from '@angular/core'; // Allows for creation of components
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 /**
  * Err Response interface
@@ -51,15 +51,15 @@ interface ErrResponse {
  */
 export class AppComponent implements OnInit{
 
-  someText = "Results: "
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    this.httpClient.get<ErrResponse>('localhost:8080').
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    this.httpClient.get('http://localhost:8080/api/search/apps/a',{ headers}).
     subscribe(
       (data) => {
       // Read the result field from the JSON response.
-      console.log(data.err);
+      console.log(data);
     },
     (err) => {
       console.log(err)
@@ -67,27 +67,27 @@ export class AppComponent implements OnInit{
 
   }
 
-  // appData = [
-  //   {
-  //     title: 'Facebook',
-  //     leeks: 40,
-  //     usrRating: 4.8,
-  //     description: 'The Facebook app does more than help you stay connected with your friends and interests. It\'s also your personal organizer for storing, saving and sharing photos. It\'s easy to share photos straight from your Android camera, and you have full control over your photos and privacy settings. You can choose when to keep individual photos private or even set up a secret photo album to control who sees it.',
-  //     image: '../assets/images/facebook.png'
-  //   },
-  //   {
-  //     title: 'Twitter',
-  //     leeks: 28,
-  //     usrRating: 4.5,
-  //     description: 'From breaking news and entertainment to sports, politics, and everyday interests, when it happens in the world, it happens on Twitter first. See all sides of the story. Join the conversation. Watch live streaming events. Twitter is what’s happening in the world and what people are talking about right now.',
-  //     image: '../assets/images/twitter.png'
-  //   },
-  //   {
-  //     title: 'Sociam Social',
-  //     leeks: 2,
-  //     usrRating: 3.8,
-  //     description: 'A Social media playform that complies with the Ethical Data Initiative\'s data handling guidelines, the platform allows you to connect with and share content with friends. All Whilst not giving away your personal information to the highest bidder.',
-  //     image: '../assets/images/sociam_social.png'
-  //   }
-  // ];
+  appData = [
+    {
+      title: 'Facebook',
+      leeks: 40,
+      usrRating: 4.8,
+      description: 'The Facebook app does more than help you stay connected with your friends and interests. It\'s also your personal organizer for storing, saving and sharing photos. It\'s easy to share photos straight from your Android camera, and you have full control over your photos and privacy settings. You can choose when to keep individual photos private or even set up a secret photo album to control who sees it.',
+      image: '../assets/images/facebook.png'
+    },
+    {
+      title: 'Twitter',
+      leeks: 28,
+      usrRating: 4.5,
+      description: 'From breaking news and entertainment to sports, politics, and everyday interests, when it happens in the world, it happens on Twitter first. See all sides of the story. Join the conversation. Watch live streaming events. Twitter is what’s happening in the world and what people are talking about right now.',
+      image: '../assets/images/twitter.png'
+    },
+    {
+      title: 'Sociam Social',
+      leeks: 2,
+      usrRating: 3.8,
+      description: 'A Social media playform that complies with the Ethical Data Initiative\'s data handling guidelines, the platform allows you to connect with and share content with friends. All Whilst not giving away your personal information to the highest bidder.',
+      image: '../assets/images/sociam_social.png'
+    }
+  ];
 }
