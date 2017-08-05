76a89a9b56b9fb19bf4fcdbad893b13c77aff14d
Adam Slack <adamslack@outlook.com>
2017-08-05 22:53:43 +0100

    Added bootstrap to the project

--- elm/edi.elm
+++ elm/edi.elm
@@ -1,22 +1,47 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+-- Read all about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/user_input/text_fields.html
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String
+import Bootstrap.CDN as CDN
+import Bootstrap.Form.Input as Input
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Card as Card
 
 main =
-    Html.beginnerProgram { model = 0, view = view, update = update}
+  beginnerProgram { model = "", view = view, update = update }
 
-type Msg = Increment | Decrement
 
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
+-- UPDATE
 
-        Decrement ->
-            model - 1
+type Msg = NewContent String
 
-view model =
-    div []
-        [ button [ onClick Decrement ] [text "-"]
-        , div[] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+"]
-        ]
+update (NewContent content) oldContent =
+  content
+
+
+-- VIEW
+
+view content =
+    Grid.container []
+    [ CDN.stylesheet
+    , Grid.row []
+        [ Grid.col [] [ h1[] [ text "EDI" ] ] ]
+    , Grid.row []
+        [ Grid.col [] [ h2[] [ text "Ethical Data Initiative" ] ] ]
+    , Grid.row []
+        [ Grid.col [] [Input.text
+                           [ Input.attrs [ placeholder "Search for an App"
+                                         , style [ ( "text-align", "center" ) ]
+                                         ]
+                           , Input.large
+                           ] ] ]
+    , Card.config []
+        |> Card.block []
+            [ Card.text [] [ text "This is some text within a card block." ] ]
+        |> Card.view
+    ]
