e58300bc8f5ebba567da0f4b3dbeb242222f5bea
Adam Slack <adamslack@outlook.com>
2017-08-25 22:00:31 +0100

    fixes

--- the-edi/src/app/host-count-bar-chart/host-count-bar-chart.component.ts
+++ the-edi/src/app/host-count-bar-chart/host-count-bar-chart.component.ts
@@ -1,6 +1,8 @@
 // Following http://blog.tomscholten.nl/make-a-bar-chart-in-angular-4-using-d3-js/
 
 import { Component, OnInit, ViewChild, Input, ElementRef } from '@angular/core';
+import { SelectionTrackingService } from '../service/selection-tracking.service';
+import { FullApp } from '../service/app-info-types.service';
 import * as d3 from 'd3';
 
 @Component({
@@ -22,9 +24,20 @@ export class HostCountBarChartComponent implements OnInit {
   chartWidth: number = 200;
   chartHeight: number = 200;
 
-  constructor() { }
+  constructor(private appTracker: SelectionTrackingService) { }
+
+  buildDataset(selection: FullApp[]): void {
+    
+  }
 
   ngOnInit(): void {
+
+    this.appTracker.currentSelectionChanged.subscribe((data) => {
+      let selection = this.appTracker.getSelections();
+      let appData = Array.from(selection.keys()).map((key) => selection.get(key));
+
+      this.buildDataset(appData);
+    });
     // Select the HTMl SVG Element from the template
     var svg = d3.select(this.chart.nativeElement);
 
