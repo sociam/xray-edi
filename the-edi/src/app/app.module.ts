42eb49ae70c2e80f6ac3b0c4929ebbf8f2ef0721
Adam Slack <adamslack@outlook.com>
2017-08-26 12:28:41 +0100

    other EDI pages set up and Routing started.

--- the-edi/src/app/app.module.ts
+++ the-edi/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 // HttpClientModule for issueing http requests.
 import { HttpClientModule } from '@angular/common/http';
-
+// Routing Modules for routing on button presses.
+import { RouterModule, Routes } from '@angular/router';
 
 // Components
 import { AppComponent } from './app.component';
@@ -12,12 +13,27 @@ import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { AppDisplayComponent } from './app-display/app-display.component';
+import { HostCountBarChartComponent } from './host-count-bar-chart/host-count-bar-chart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 // Services
 import { AppInfoTypesService } from './service/app-info-types.service';
 import { XrayAPIService } from './service/xray-api.service';
 import { SelectionTrackingService } from './service/selection-tracking.service';
-import { HostCountBarChartComponent } from './host-count-bar-chart/host-count-bar-chart.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about',component: AboutComponent },
+  { path: 'refine',component: AppDisplayComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -26,13 +42,20 @@ import { HostCountBarChartComponent } from './host-count-bar-chart/host-count-ba
     NavbarComponent,
     SidenavComponent,
     AppDisplayComponent,
-    HostCountBarChartComponent
+    HostCountBarChartComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [AppInfoTypesService, XrayAPIService, SelectionTrackingService],
   bootstrap: [AppComponent,AutocompleteComponent, SidenavComponent, NavbarComponent, AppDisplayComponent]
