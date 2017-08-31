519075a61bec3073cda189e9c3b1f8efe894936c
Adam Slack <adamslack@outlook.com>
2017-08-31 14:39:42 +0100

    Tooltip for donut

--- the-edi/src/app/host-company-donut/host-company-donut.component.ts
+++ the-edi/src/app/host-company-donut/host-company-donut.component.ts
@@ -17,7 +17,7 @@ export class HostCompanyDonutComponent implements OnInit {
 
   @ViewChild('chart') chart: ElementRef;
   private child: ElementRef;
-  private dataset: any = [{label:'', value: 0}];
+  private dataset: any = [{label:'', value: 0, app: []}];
 
   // https://bl.ocks.org/mbostock/4062045
   private chartWidth: number;
@@ -36,25 +36,34 @@ export class HostCompanyDonutComponent implements OnInit {
 
   buildDataset(selection: FullApp[]) {
     // [ ...,
-    //  { label: facebook, value: n / N },
+    //  { label: facebook, value: n / N, apps: [facebook, cats, snapchat,]},
     //  ... ]
     let bigN = 0
     let companies = selection.map((app, idx) => {
       bigN += app.hosts.length;
       return app.hosts.map((host) => {
         let mapping = this.companyLookup.getCompanyFromDomain(host).map((company: CompanyInfo) => company.id );
+        let pair = {company: '', app: ''}
         if (mapping.length == 0) {
-          mapping = ['Uknown'];
+          return {company: 'Unknown', app: app.storeinfo.title}
         }
-        return mapping;
+        return {company: mapping[0], app: app.storeinfo.title};
       })
     }).reduce((a,b) => a.concat(b),[]);
-    let freq = _.countBy(companies, _.identity);
+    let freq = _.countBy(companies, 'company');
+
     if (!freq) {
       return [{label:'', value: 0}];
     }
-    return _.sortBy(_.keys(freq).map((key) =>  {return {label: key, value: freq[key]/bigN}}),'value');
-
+    let data = _.sortBy(_.keys(freq).map((key) =>  {
+      return {
+        label: key,
+        value: freq[key]/bigN,
+        apps: _.uniq(companies.filter((el) => el.company == key).map((el) => el.app))
+      }
+    }),'value');
+  console.log(data);
+  return data;
   }
 
   buildGraph(dataset) {
@@ -86,7 +95,9 @@ export class HostCompanyDonutComponent implements OnInit {
                 .outerRadius(radius * 0.8)
 	              .innerRadius(radius * 0.4)
                 .startAngle(function(d) { return d.startAngle + Math.PI/3.333; })
-                .endAngle(function(d) { return d.endAngle + Math.PI/3.333; });
+                .endAngle(function(d) { return d.endAngle + Math.PI/3.333; })
+
+                
     var outerArc = d3.arc()
                      .innerRadius(radius * 0.9)
                      .outerRadius(radius * 0.9)
@@ -99,22 +110,51 @@ export class HostCompanyDonutComponent implements OnInit {
     var key = (d) => {return d.data.label};
 
     var colour = d3.scaleOrdinal(d3.schemeCategory20);
-    let data = this.dataset;
+    var data = this.dataset;
     var slice = svg.select('.slices').selectAll('path.slice')
          .data(pie(data), key)
          .enter()
          .insert('path')
          .style('fill', (d) => { return colour(d.data.label) })
          .attr('class', slice);
+         
     slice.transition().duration(1000)
 		     .attrTween('d', function(d) {
            this._current = this._current || d;
-           var interpolate = d3.interpolate(this._current, d);
+           let interpolate = d3.interpolate(this._current, d);
            this._current = interpolate(0);
            return function(t) {
              return arc(d);
            };
       })
+      
+    var div = d3.select("body").append("div").attr("class", "toolTip");
+    div
+    .style('position', 'absolute')
+    .style('display', 'none')
+    .style('width', 'auto')
+    .style('height', 'auto')
+    .style('background', '#222')
+    .style('border', '0 none')
+    .style('border', 'radius 8px 8px 8px 8px')
+    .style('box-shadow', '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)')
+    .style('color', '#fff')
+    .style('font-size', '0.75em')
+    .style('padding', '5px')
+    .style('text-align', 'left');
+
+    slice
+        .on("mousemove", (d) => {
+            div.style("left", d3.event.pageX+10+"px");
+            div.style("top", d3.event.pageY-25+"px");
+            div.style("display", "inline-block");
+            div.html(d.data.apps.reduce((a,b) => a+'<br>'+b));
+        });
+
+    slice
+        .on("mouseout", function(d){
+            div.style("display", "none");
+        });
     slice.exit()
          .remove();
          
@@ -170,6 +210,7 @@ export class HostCompanyDonutComponent implements OnInit {
   }
 
   private graphInit() {
+    /* This makes me wanna Vom. pls forgive. this was a quick fix, cba to fix it up, have other things to do. */
     if(this.onlySingle) {
           let selection = this.appTracker.getCurrentSelection();
           if(selection) {
@@ -201,7 +242,7 @@ export class HostCompanyDonutComponent implements OnInit {
   }
   ngOnInit(): void {
     this.companyLookup.parseCompanyInfo();
-    this.companyLookup.companyInfoParsed.subscribe((d) => {this.graphInit(); this.graphInit()});
+    this.companyLookup.companyInfoParsed.subscribe((d) => {this.graphInit()});
     // Select the HTMl SVG Element from the template
   }
 
