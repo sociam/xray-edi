24ef87c9f2fbec7a8fcf7b53637994004c7688c8
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-09-06 22:05:25 +0100

    Added the company setup wanted

--- the-edi/src/app/app-inspect/app-inspect.component.ts
+++ the-edi/src/app/app-inspect/app-inspect.component.ts
@@ -4,6 +4,7 @@ import { FullApp } from '../service/app-info-types.service';
 import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 import { XrayAPIService } from '../service/xray-api.service';
 import { Subscription } from 'rxjs/Subscription';
+import { CompanyInfoService, CompanyInfo } from '../service/company-info.service';
 
 enum dangerZone {
   dangerous,
@@ -11,13 +12,11 @@ enum dangerZone {
   normal
 }
 
-
 export class Perms {
   level: dangerZone
   description: string
   name: string
 }
-
 @Component({
   selector: 'app-app-inspect',
   templateUrl: './app-inspect.component.html',
@@ -36,15 +35,18 @@ export class AppInspectComponent implements OnInit {
 
   private currentSubscription: Subscription;
   private selectionSubscription: Subscription;
+
+
   public downloads: string = '0';
 
   removeApp(id: string) {
     this.appTracker.removeApp(id);
   }
   constructor(private appTracker: SelectionTrackingService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private xrayAPI: XrayAPIService) {
+    private route: ActivatedRoute,
+    private router: Router,
+    private xrayAPI: XrayAPIService,
+    private companyLookup: CompanyInfoService) {
     router.events.subscribe((event) => {
       if (event instanceof NavigationStart && event.url.split('/')[1] != 'apps') {
         this.currentSubscription.unsubscribe();
@@ -52,19 +54,19 @@ export class AppInspectComponent implements OnInit {
       }
     })
   }
+
   setCompare(app: FullApp) {
     this.currentAltTitle = app.storeinfo.title;
     this.appTracker.setCompareSelection(app);
   }
 
-
-
   ngOnInit() {
     this.route.params.subscribe((param) => {
-      if(param.app && param.app) {
-        this.xrayAPI.fetchApps({fullInfo: true, limit:10, appID: param.app}).subscribe((app) => {
+
+      if (param.app && param.app) {
+        this.xrayAPI.fetchApps({ fullInfo: true, limit: 10, appID: param.app }).subscribe((app) => {
           app = app.filter(element => element.app == param.app);
-          if(app.length > 0 && app[0].app == param.app) {
+          if (app.length > 0 && app[0].app == param.app) {
             this.appTracker.setCurrentSelection(app[0]);
           }
           else {
@@ -76,21 +78,52 @@ export class AppInspectComponent implements OnInit {
           this.altApps = [];
           alts.map((alt) => alt.then((app) => this.altApps.push(app)));
         });
+
       }
     });
 
     this.currentSubscription = this.appTracker.currentSelectionChanged.subscribe((data) => {
       this.currentSelection = this.appTracker.getCurrentSelection();
       this.selectionMade = true;
-      this.test = JSON.stringify(this.currentSelection,null,'  ');
+      this.test = JSON.stringify(this.currentSelection, null, '  ');
       this.downloads = this.currentSelection.storeinfo.installs.max.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
       this.router.navigate(['apps/' + this.currentSelection.app]);
+
+
+      var companiesToHosts = this.loadCompanyHosts(this.currentSelection);
+
+      console.log( companiesToHosts);
+      var a3 = companiesToHosts.reduce((acc, x) => {
+        //var combinedHosts =  acc[x.company].concat(x.host);
+        console.log("Value", acc[x.company])
+        acc[x.company] = acc[x.company] ? acc[x.company].concat([x.host])  || {} : [x.host];
+        return acc;
+      }, {});
+      console.log(a3);
+
     });
 
     this.selectionSubscription = this.appTracker.appSelectionsChanged.subscribe((data) => {
       this.allSelections = this.appTracker.getSelections();
-      this.selectionValues = Array.from(this.allSelections.keys()).map(key=>this.allSelections.get(key));
+      this.selectionValues = Array.from(this.allSelections.keys()).map(key => this.allSelections.get(key));
     });
+
+
+
   }
+
+  private loadCompanyHosts(app: FullApp) {
+    return app.hosts.map((host) => {
+      let mapping = this.companyLookup.getCompanyFromDomain(host).map((company: CompanyInfo) => company.id);
+      let pair = { company: '', app: '' }
+
+      if (mapping.length == 0) {
+        return { company: 'Unknown', host: host }
+      }
+      return { company: mapping[0], host: host };
+    }); //.reduce((a,b) => a.concat(b),[]);
+
+  }
+
 }
